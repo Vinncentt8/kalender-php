62de8369371831e667001f71c78fa435dac85af9
VIN-PC\vince <99008212@mydavinci.nl>
2018-01-06 22:56:36 +0100

    .

--- arrays.php
+++ arrays.php
@@ -4,9 +4,9 @@ $color = array("Blue","Orange","Red");
 
 $day = array('1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31');
 
-$month = array("Januari","Februari","March","April","May","June","July","August","September","Oktober","November","December");
+$month = array("January","February","March","April","May","June","July","August","September","October","November","December");
 
-$year = array("2018","2019","2020","2021","2022","2023","2024","2025");
+$year = array("2018","2019","2020");
 
 $daysOfWeek = array('Sun','Mon','Tue','Wen','Tur','Fri','Sat');
 
@@ -14,3 +14,5 @@ $daysOfWeek = array('Sun','Mon','Tue','Wen','Tur','Fri','Sat');
 $currentdate = date('d-m-Y');
 
 ?>
+
+<!-- ,"2021","2022","2023","2024","2025","2026","2027","2028","2029","2030" -->
--- index.php
+++ index.php
@@ -3,6 +3,8 @@ include 'connection.php';
 include 'arrays.php';
 ?>
 
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,8 +23,8 @@ include 'arrays.php';
 								<input type="submit">
 									<br>
 									<br>
-									<select name="month">
-										<option>Select month</option>
+										<select name="month">
+											<option>Select month</option>
 
 											<?php
 											
@@ -32,10 +34,10 @@ include 'arrays.php';
 		    								?>
 
 
-									</select>
+										</select>
 
 										<select name="day">
-										<option>Select day</option>
+											<option>Select day</option>
 
 											<?php
 											
@@ -45,7 +47,18 @@ include 'arrays.php';
 		    								?>
 
 
-									</select>											
+									</select>
+
+										<select name="year">
+											<option>Select year</option>
+
+											<?php 
+												foreach ($year as $value) {
+													echo "<option value=\"" . $year . "\">" . $value . "</option>";		
+												}
+
+											?>	
+										</select>											
 								</form>
 							</div>
 						</div>
@@ -73,66 +86,68 @@ include 'arrays.php';
             <!-- $today = date('\i\t \i\s \t\h\e jS \d\a\y.');   // it is the 10th day. -->
             <!-- Nu de dagen van de maand nog en dan de Table's invoegen -->
             <p id="date"  style="background-color: grey;">
+
+
             <?php
    				echo( date( 'l j',    time() ));
    			?>
    			</p>
             <tr>
-            	<td>26</td>
-            	<td>27</td>
-            	<td>28</td>
-            	<td>29</td>
-            	<td>30</td>
-            	<td>1</td>
-            	<td>2</td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
             </tr>
 
             <tr>
-            	<td>3</td>
-            	<td>4</td>
-            	<td>5</td>
-            	<td>6</td>
-            	<td>7</td>
-            	<td>8</td>
-            	<td>9</td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
             </tr>
 
             <tr>
-            	<td>10</td>
-            	<td>11</td>
-            	<td>12</td>
-            	<td>13</td>
-            	<td>14</td>
-            	<td>15</td>
-            	<td>16</td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
             </tr>
 
             <tr>
-            	<td>16</td>
-            	<td>18</td>
-	         	<td>19</td>
-            	<td>20</td>
-            	<td>21</td>
-            	<td>22</td>
-            	<td>23</td>
+            	<td></td>
+            	<td></td>
+	         	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
             </tr>
 
             <tr>
-            	<td>24</td>
-            	<td>25</td>
-            	<td>26</td>
-            	<td>27</td>
-            	<td>28</td>
-            	<td>29</td>
-            	<td>30</td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
             </tr>
             <tr>
-            	<td>31</td>
-            	<td>1</td>
-            	<td>2</td>
-            	<td>3</td>
-            	<td>4</td>
-            	<td>5</td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
+            	<td></td>
             	<td></td>
             </tr>
         </table>
