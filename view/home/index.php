6a2ad218e778b5888370059785e76d3fd770e97f
VIN-PC\vince <99008212@mydavinci.nl>
2018-02-17 20:12:06 +0100

    2

--- view/home/index.php
+++ view/home/index.php
@@ -1,12 +1,8 @@
 <div class="container">
  <h1>Welkom bij kalender.</h1>
- <p>Je bent nu in home/index.</p>
+ <br>
 </div>
 
-<html>
-						<form action="send" method="post">
-							<input type="text" name="name" placeholder="Name">
-							<input type="date" name="Geboorte">
-							<input type="submit" name="submit" value="Register Birthdate">
-						</form>
-</html>
+
+<a href="//localhost/Framework-php-/calendar">Kalender</a>
+
